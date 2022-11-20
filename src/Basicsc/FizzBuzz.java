19f32ee541c658c50c364fd8a5e7acc945e5bdd4
steliancreanga <steliancreanga@gmail.com>
2022-11-20 23:28:37 +0200

    RizzJazz

--- src/Basicsc/FizzBuzz.java
+++ src/Basicsc/FizzBuzz.java
@@ -3,6 +3,7 @@ package Basicsc;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
+
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FIZZBUZZ ");
                 continue;
@@ -15,6 +16,16 @@ public class FizzBuzz {
                 System.out.println("BUZZ ");
                 continue;
             }
+
+            if (i % 7 == 0) {
+                System.out.println("RIZZ ");
+                continue;
+            }
+            if (i % 11 == 0) {
+                System.out.println("JAZZ ");
+                continue;
+            }
+
         }
     }
 }
