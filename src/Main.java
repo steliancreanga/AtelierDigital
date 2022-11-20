c7ffb131d97696712b338f75e2c995afc13864e8
steliancreanga <steliancreanga@gmail.com>
2022-11-20 23:04:45 +0200

    basicsc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("args =" + Arrays.deepToString(args));
     }
 }
